eb10bcfa6586953ec3f66b2d5926aedd63944707
Fahim Chowdhury <fahim.chowdhury@ogilvy.com>
2013-06-12 18:02:27 +0100

    init

--- quiz/main.js
+++ quiz/main.js
@@ -1,7 +1,8 @@
 (function(window) {
 	var data = {};
 	var email = "fahim@gmx.com";
-	var answers=[];
+	var currentIndex = 0;
+	var answers = [];
 	function Main() {
 		if (window.addEventListener) {
 			window.addEventListener("load", onLoad);
@@ -13,17 +14,102 @@
 
 	function onLoad() {
 		// check shape class
-		Utensil.URLLoader.load("resource/data/quiz1.json",onJSONLoaded)
+		Utensil.URLLoader.load("resource/data/quiz1.json", onJSONLoaded);
+
 	}
-	function onJSONLoaded(t,x)
-	{
-		data = eval("("+t+")");
+
+	function onJSONLoaded(t, x) {
+		data = eval("(" + t + ")");
 		console.log(data);
+		buildQuestion();
+
+		Utensil.addListener(document.getElementById("nextButton"), "click", onNextClick);
 	}
-	function buildQuestion()
-	{
-		
+
+	function onNextClick(event) {
+		checkAnswer();
+		currentIndex++;
+		buildQuestion();
 	}
+
+	function checkAnswer() {
+		switch(data.questions[currentIndex].type) {
+			case "radio":
+				checkRadio();
+				break;
+			case "code":
+				checkCode();
+
+		}
+	}
+
+	function checkRadio() {
+		var radios = document.getElementsByName('answer');
+
+		for (var i = 0, length = radios.length; i < length; i++) {
+			if (radios[i].checked) {
+				console.log(radios[i].value);
+			}
+		}
+	}
+
+	function checkCode() {
+		var answer =data.questions[currentIndex].answers[0];
+		var str =document.getElementById("answerText");
+		var arr = answer.split(",");
+		var found = 0;
+		for(var a=0;a<arr.length;a++)
+		{
+			if(str.value.indexOf(arr[a])>=0)
+			{
+				found++;
+			}
+		}
+		console.log(found,arr.length);
+	}
+
+	function buildQuestion() {
+		document.getElementById("questionCopy").innerHTML = "";
+		document.getElementById("answers").innerHTML = "";
+		if (data.questions[currentIndex]) {
+			document.getElementById("questionCopy").innerHTML = data.questions[currentIndex].question;
+			switch(data.questions[currentIndex].type) {
+				case "radio":
+					buildRadio();
+					break;
+				case "code":
+					buildText();
+
+			}
+		}
+	}
+
+	function buildRadio() {
+		for (var a = 0; a < data.questions[currentIndex].answers.length; a++) {
+			var answer = data.questions[currentIndex].answers[a];
+			var li = document.createElement("LI");
+			var radio = document.createElement("INPUT");
+			var p = document.createElement("P");
+			radio.type = "radio";
+			if (a == 0)
+				radio.checked = "checked";
+			radio.name = "answer";
+			p.innerHTML = answer;
+			radio.value = a;
+			li.appendChild(radio);
+			li.appendChild(p);
+			document.getElementById("answers").appendChild(li);
+		}
+	}
+
+	function buildText() {
+		var li = document.createElement("LI");
+		var text = document.createElement("TEXTAREA");
+		text.id="answerText";
+		li.appendChild(text);
+		document.getElementById("answers").appendChild(li);
+	}
+
 	Main();
 }
 )(window);
