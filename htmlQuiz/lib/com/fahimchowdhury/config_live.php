84043b565f04348a27865db2ada6347fb118ab3e
Fahim Chowdhury <fahim.chowdhury@ogilvy.com>
2013-06-21 15:56:25 +0100

    added html quiz

---
+++ htmlQuiz/lib/com/fahimchowdhury/config_live.php
@@ -0,0 +1,7 @@
+<?php
+define('HOST', 'localhost');
+define('DATABASE', 'fahimcho_javascript_quiz');
+define('USER', 'fahimcho_quiz');
+define('PASS', '20130613');
+
+?>
---
+++ htmlQuiz/lib/com/fahimchowdhury/submit.php
@@ -0,0 +1,71 @@
+<?php
+$data = stripcslashes($_POST["answers"]);
+$email = stripcslashes($_POST["email"]);
+$firstName = stripcslashes($_POST["firstname"]);
+$lastName = stripcslashes($_POST["lastname"]);
+$email = stripcslashes($_POST["email"]);
+$minutes = stripcslashes($_POST["minutes"]);
+$seconds = stripcslashes($_POST["seconds"]);
+if (isset($data) && isset($email)) {
+	// include_once 'config.php';
+	$content = file_get_contents('../../../resource/data/quiz1_answers.json');
+	$answersJson = json_decode($content);
+	$answersJson = $answersJson->answers;
+	//echo $data;
+	$data = explode(",", $data);
+	$correctCount = 0;
+	for ($a = 0; $a < count($data); $a++) {
+		$answer = $data[$a];
+		$currentAnswer=$answersJson[$a];
+		switch($currentAnswer->type) {
+			case "radio" :
+				if ($answer == $currentAnswer->answer)
+					$correctCount++;
+				break;
+			case "code" :
+				if (checkCode($currentAnswer->answer, $answer)) {
+					$correctCount++;
+				}
+				break;
+		}
+	}
+	$percentage = ($correctCount / count($data)) * 100;
+	$date = date('Y-m-d H:i:s', '1299762201428');
+	// $con = mysqli_connect(HOST, USER, PASS, DATABASE);
+	// // Check connection
+	// if (mysqli_connect_errno($con)) {
+		// echo "<script>window.parent.submitComplete(-1)</script>";
+	// } else {
+		// mysqli_query($con, "INSERT INTO results (first_name, last_name, email,minutes,seconds,percentage,date)
+// VALUES ('$firstName', '$lastName','$email','$minutes','$seconds','$percentage','$date')");
+
+$to      = $email;
+$subject = 'HTML Quiz';
+$message = "Hi ".$firstName." ".$lastName.',\r\nHere is your quiz result. \r\n'.$percentage."%";
+$headers = 'From: fahim.chowdhury1985@gmail.com' . "\r\n" .
+    'Reply-To: fahim.chowdhury1985@gmail.com' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+mail($to, $subject, $message, $headers);
+		echo "<script>window.parent.submitComplete($percentage)</script>";
+	// }
+// 
+	// mysqli_close($con);
+} else {
+	echo "<script>window.parent.submitComplete(-1)</script>";
+}
+function checkCode($correct, $answer) {
+	$arr = explode(",", $correct);
+	$found = 0;
+	for ($a = 0; $a < count($arr); $a++) {
+		if (strpos($answer, $arr[$a]) !== false) {
+			$found++;
+		}
+	}
+	if ($found == count($arr)) {
+		return true;
+	} else {
+		return false;
+	}
+}
+?>
